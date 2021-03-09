81c5d48cbf4398c96dff82d386055981f722993c
spartazhc <spartazhc@gmail.com>
2021-03-09 16:16:00 +0800

    hw1 submit
    Signed-off-by: spartazhc <spartazhc@gmail.com>

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -76,8 +76,15 @@ class ReplayBuffer(object):
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
+        idx = np.random.choice(self.obs.shape[0], batch_size, replace=False)
 
-        return TODO, TODO, TODO, TODO, TODO
+        return (
+            self.obs[idx],
+            self.acs[idx],
+            self.rews[idx],
+            self.next_obs[idx],
+            self.terminals[idx],
+        )
 
     def sample_recent_data(self, batch_size=1):
         return (
--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -81,7 +81,10 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             observation = obs[None]
 
         # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        observation_tensor = torch.tensor(observation, dtype=torch.float).to(ptu.device)
+        action_distribution = self.forward(observation_tensor)
+
+        return action_distribution.sample().cpu().detach().numpy()
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -93,7 +96,13 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError
+        if self.discrete:
+            return distributions.Categorical(logits=self.logits_na(observation))
+        else:
+            return distributions.Normal(
+                self.mean_net(observation),
+                torch.exp(self.logstd)[None],
+                )
 
 
 #####################################################
@@ -109,7 +118,22 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+        observation_tensor = torch.tensor(
+            observations,
+            device=ptu.device,
+            dtype=torch.float
+            )
+        action_tensor = torch.tensor(
+            actions,
+            device=ptu.device,
+            dtype=torch.int if self.discrete else torch.float
+            )
+        action_distribution = self(observation_tensor)
+
+        loss = -action_distribution.log_prob(action_tensor).mean()
+        loss.backward()
+        self.optimizer.step()
+
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
